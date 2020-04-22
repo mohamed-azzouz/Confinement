692597731e5246ca74290f9057503e2b51bd782a
serge <serge.moi@laplateforme.io>
2020-04-22 17:51:02 +0200

    html css index fin

--- index.php
+++ index.php
@@ -11,9 +11,16 @@ require("templates/header1.phtml"); ?>
 					<button type="submit" id="but-crea-sal">Créer un salon</button>
 				</form>
 				<img src="img/serjuani2.png" alt="youtube" id="yout-img">
-				<div id="div-form-absolute"></div>
 			</section>
 		</div>
+		<article id="article-index">
+			<h2 id="index-title2">Les fonctionnalités de Covidéo-19</h2>
+			<div id="box-index-para">
+				<p>- Un lecteur synchronisé pour l'audio et la vidéo.</p>
+				<p>- Parler avec vos amis dans des salons grâce au chat intégré.</p>
+				<p>- Profiter du contenu de YouTube avec vos amis.</p>
+			</div>
+		</article>
 	</main>
 </div>
 
