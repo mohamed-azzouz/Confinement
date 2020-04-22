5e89602c91a2b0214db072bdda71b5dbdb2ae3d4
serge <serge.moi@laplateforme.io>
2020-04-22 16:24:52 +0200

    avancement html css index

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>INDEX</title>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+session_start();
+
+require("templates/header1.phtml"); ?>
+
+	<main id="main-index">
+		<div id="main-div">
+			<section id="sec1-index">
+				<form action="" method="post" id="form-index">
+					<button type="submit" id="but-crea-sal">Créer un salon</button>
+				</form>
+				<img src="img/serjuani2.png" alt="youtube" id="yout-img">
+				<div id="div-form-absolute"></div>
+			</section>
+		</div>
+	</main>
+</div>
+
+<?php require("templates/footer.phtml") 
+
+
+
+
+
+?>
