942e47295d04c57f4214c855332f3d3cfb47f2a5
serge <serge.moi@laplateforme.io>
2020-04-23 22:34:29 +0200

    fin css salon et index

--- salon.php
+++ salon.php
@@ -26,36 +26,22 @@ if (isset($_GET['id']))
 ?>
 
 
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="jquery.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body id="visionage">
-<?php require("templates/header1.phtml"); ?>
-    <main>
-        <form class ="rechercher" action="" method="POST">
-            <input type="text" id="nav" placeholder="Lien Youtube">
-            <input type="submit" value="Rechercher">
+<?php require("templates/header2.phtml"); ?>
+    <main id="main-salon">
+        <form id="rechercher" action="" method="POST">
+            <div id="box-imgyout">
+                <img id="yout" src="img/yout.png" alt="">
+            </div>
+            <input type="text" id="inp-search" placeholder="Lien Youtube">
+            <input type="submit" id="inpsub-search" value="search">
         </form>
-        <div class="box">
+        <div id="box">
             <div id="video">
-                <iframe width="900" height="500" src="https://www.youtube.com/embed/BWG29Kss-rY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="900" height="500" src="https://www.youtube.com/embed/EY5ac2ujJtQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <section id="chatbox">
-                <div id="historique">
-                    Historique Chat
-                </div>
-                <div id="utilisateurs">
-                    Utilisateurs dans le salon : Serjuani, Mohamed, Paul, Raph.
-                </div>
                 <div id="messages">
-                    Messages :<br><br>
                     <?php
 
                     $requeteMessage = "SELECT * FROM messages INNER JOIN utilisateurs ON utilisateurs.id = messages.id_utilisateur WHERE id_salon = '".$_GET['id']."'";
@@ -63,7 +49,7 @@ if (isset($_GET['id']))
                     $resultMessage = mysqli_fetch_all($queryMessage);
 
                     foreach($resultMessage as $messages): ?>
-                        <div><?php echo $messages[5].':'.$messages[3]; ?></div>
+                        <div id="mess"><?php echo $messages[5].': '.$messages[3]; ?></div>
                     <?php endforeach; ?>
                 </div>
                     <?php
@@ -88,17 +74,12 @@ if (isset($_GET['id']))
 
 
                     <div id="bouton">
-                        <form class ="send" action="" method="post">
+                        <form id="send" action="" method="post">
                             <input type="text" name="message" id="envoyer" placeholder="Écrire un message" required>
                             <input onClick="load_messages(); return false;" type="submit" name="sendMessage"  value="Send">
                         </form>
                     </div>
                 </section>
-                <div class="hide">Three
-                    <br>has
-                    <br>extra
-                    <br>text
-                </div>
             </div>
 
         </main>
@@ -106,7 +87,7 @@ if (isset($_GET['id']))
         <script>
             setInterval("load_messages()", 500);
             function load_messages(messages){
-                $("#messages").load("salon.php?id='".$_GET['id']."' #messages");
+                $("#messages").load("salon.php #messages");
             }
         </script>
     </body>
