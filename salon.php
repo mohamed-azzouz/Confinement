9819c205481cfc7d01a23edcc42480a3278b2701
serge <serge.moi@laplateforme.io>
2020-04-24 10:17:16 +0200

    histo

--- salon.php
+++ salon.php
@@ -15,6 +15,22 @@ if (isset($_GET['id']))
     $requeteUser="SELECT * from utilisateurs WHERE id = '".$_SESSION['id']."'";
     $queryUser = mysqli_query($connexion,$requeteUser);
     $resultatUser = mysqli_fetch_assoc($queryUser);
+
+
+
+
+if (isset($_POST['lien']) AND !empty($_POST['lien'])) 
+{
+ 	$newLien = "INSERT INTO historique(id_utilisateur, id_salon, lien) VALUES ('".$_SESSION['id']."', '".$_GET['id']."', '".$_POST['lien']."')";
+ 	$queryNewLien = mysqli_query($connexion, $newLien);
+
+ 	echo $newLien;
+}
+
+$requeteHistorique = "SELECT * FROM historique INNER JOIN utilisateurs ON utilisateurs.id = historique.id_utilisateur INNER JOIN salon ON salon.id = historique.id_salon WHERE id_salon = '".$_GET["id"]."'";
+$queryHistorique = mysqli_query($connexion, $requeteHistorique);
+$resultHistorique = mysqli_fetch_all($queryHistorique);
+
 }
 
 if(!isset($_SESSION["login"])){
@@ -39,6 +55,12 @@ if(!isset($_SESSION["login"])){
             <div id="video">
                 <iframe width="900" height="500" src="https://www.youtube.com/embed/EY5ac2ujJtQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
+            <section id="historique">
+                <?php foreach($resultHistorique as $lien): ?>
+                    <div><?php echo "User : $lien[5]" ?></div><div><?php echo "Salon : $lien[10]"?></div>
+                    <a href="<?php echo $lien[3]; ?>"><?php echo $lien[3]; ?></a>
+                <?php endforeach; ?>
+            </section>
             <section id="chatbox">
                 <div id="messages">
                     <?php
