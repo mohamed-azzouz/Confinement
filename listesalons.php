7b800b67725f0e376b6a399a5c13813211478b4b
paul-le <paulloukyle@gmail.com>
2020-04-24 09:46:49 +0200

    Ajout listesalons.php

--- listesalons.php
+++ listesalons.php
@@ -1,16 +1,31 @@
 <?php
 session_start();
 include('fonctions.php');
+
+$connexion = mysqli_connect('Localhost','root','','confinement');
+
+$requeteListeSalons = "SELECT * FROM salon AS sal INNER JOIN utilisateurs AS uti ON sal.id_utilisateur =  uti.id";
+$queryListeSalons = mysqli_query($connexion,$requeteListeSalons);
+$resultatListeSalons = mysqli_fetch_all($queryListeSalons);
+$countListeSalons = count($resultatListeSalons);
+
+var_dump($resultatListeSalons);
+
 require("templates/header2.php"); ?>
 
     <main id="listeSalonMain">
         <section id="listeSalonTop">
-            <article>
+            <article id="topPageArt">
                 Liste des salons
             </article>
-        </section>
-        <section id="generationSalonSection">
-            
+                <?php 
+                    $i = 0;
+                    while($i != $countListeSalons)
+                    { ?>
+                        <div id="salonDivGen">
+                            <?php echo $resultatListeSalons[$i][2]. " | " .$resultatListeSalons[$i][4]; ?>
+                        </div>
+                    <?php $i++; } ?>
         </section>
     </main>
 
